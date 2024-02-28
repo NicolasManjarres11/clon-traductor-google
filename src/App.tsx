e580721680652fc59babf43cc6406caa14c1a2c4
unknown <nicolasmanjarresg@gmail.com>
2024-02-27 21:00:29 -0500

    se agrega ícono de flechas para intercambiar el lenguaje, se crea componente para seleccionar el language, donde mapeamos la constante de lenguajes soportados pero colocandolo como parámetro en un Object.entries, para que divida los elementos dentro de esta constante y que se puedan interar correctamente dentro del formulario, se define en el archivo de tipados, los tipos específicos para lenguaje a traducir y lenguaje traducido, además, se agrega en el componente selector images, una condicional de que si el tipo es igual a from, que se agregue una opción que diga agregar automáticamente

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,53 @@
 import './App.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { Container, Row, Col, Button } from 'react-bootstrap';
+
+
 import { useStore } from './hooks/useStore';
+import { AUTO_LANGUAGE } from './constants';
+import { ArrowsIcon } from './components/icons';
+import { LanguageSelectot } from './components/LanguageSelector';
 
 
 
 
 function App() {
 
-  const {fromLanguage, setFromLanguage} = useStore() //importamos función donde está alojado el hook useReduce
+  const {fromLanguage, toLanguage, interchangeLanguajes ,setFromLanguage, setToLanguage} = useStore() //importamos función donde está alojado el hook useReduce
 
 
 
   return (
-    <div className='App'>
+    <Container fluid> {/* fluido es que se ajusta al ancho */}
       <h1>Google Translate</h1>
-      <button onClick={() => {
-        setFromLanguage('es') //solo colocamos el paylout
-      }}>Cambiar a español</button>
-      {fromLanguage}
-    </div>
+
+    <Row>
+      <Col>
+        <LanguageSelectot
+        onChange={setFromLanguage}
+        type='from'
+        value={fromLanguage}/>
+      </Col>
+
+      <Col>
+        <Button variant='link' disabled={fromLanguage === AUTO_LANGUAGE} onClick={interchangeLanguajes}>
+          <ArrowsIcon/>
+        </Button>
+      </Col>
+
+      <Col>
+        <LanguageSelectot 
+        onChange={setToLanguage}
+        type='to'
+        value={toLanguage}
+        />
+      </Col>
+
+
+    </Row>
+
+
+    </Container>
   )
 }
 
