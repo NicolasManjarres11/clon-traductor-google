87577021ae9f76f155ad64c8ea5e39aacc44ca6a
unknown <nicolasmanjarresg@gmail.com>
2024-03-02 16:37:37 -0500

    se actualiza documentación

--- src/App.tsx
+++ src/App.tsx
@@ -1,54 +1,71 @@
-import './App.css'
-import 'bootstrap/dist/css/bootstrap.min.css';
-import { Container, Row, Col, Button } from 'react-bootstrap';
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Container, Row, Col, Button, Form, Stack } from "react-bootstrap";
 
-
-import { useStore } from './hooks/useStore';
-import { AUTO_LANGUAGE } from './constants';
-import { ArrowsIcon } from './components/icons';
-import { LanguageSelectot } from './components/LanguageSelector';
-
-
-
+import { useStore } from "./hooks/useStore";
+import { AUTO_LANGUAGE } from "./constants";
+import { ArrowsIcon } from "./components/icons";
+import { LanguageSelector } from "./components/LanguageSelector";
+import { SectionType } from "./types.d";
 
 function App() {
-
-  const {fromLanguage, toLanguage, interchangeLanguajes ,setFromLanguage, setToLanguage} = useStore() //importamos función donde está alojado el hook useReduce
-
-
+  const {
+    fromLanguage,
+    toLanguage,
+    interchangeLanguajes,
+    setFromLanguage,
+    setToLanguage,
+  } = useStore(); //importamos función donde está alojado el hook useReduce
 
   return (
-    <Container fluid> {/* fluido es que se ajusta al ancho */}
-      <h1>Google Translate</h1>
+    <Container fluid>
+      {" "}
+      {/* fluido es que se ajusta al ancho */}
+      <h2>Google Translate</h2>
+      <Row>
+        <Col>
+          <Stack gap={2}>
+            <LanguageSelector
+              onChange={setFromLanguage}
+              type={SectionType.FROM}
+              value={fromLanguage}
+            />
+            <Form.Control
+              as="textarea"
+              placeholder="Introducir texto"
+              autoFocus
+              style={{ height: "150px" }}
+            ></Form.Control>
+          </Stack>
+        </Col>
 
-    <Row>
-      <Col>
-        <LanguageSelectot
-        onChange={setFromLanguage}
-        type='from'
-        value={fromLanguage}/>
-      </Col>
+        <Col xs="auto">
+          <Button
+            variant="link"
+            disabled={fromLanguage === AUTO_LANGUAGE}
+            onClick={interchangeLanguajes}
+          >
+            <ArrowsIcon />
+          </Button>
+        </Col>
 
-      <Col>
-        <Button variant='link' disabled={fromLanguage === AUTO_LANGUAGE} onClick={interchangeLanguajes}>
-          <ArrowsIcon/>
-        </Button>
-      </Col>
-
-      <Col>
-        <LanguageSelectot 
-        onChange={setToLanguage}
-        type='to'
-        value={toLanguage}
-        />
-      </Col>
-
-
-    </Row>
-
-
+        <Col>
+          <Stack gap={2}>
+            <LanguageSelector
+              onChange={setToLanguage}
+              type={SectionType.TO}
+              value={toLanguage}
+            />
+            <Form.Control
+              as="textarea"
+              placeholder="Traducción"
+              style={{ height: "150px" }}
+            ></Form.Control>
+          </Stack>
+        </Col>
+      </Row>
     </Container>
-  )
+  );
 }
 
-export default App
+export default App;
